71e211c81c7748e1160d3aad43faff7b4a208a9b
Santi <santipapel16@gmail.com>
2026-09-02 18:31:39 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,33 @@
+// ============================================================
+// FIREBASE CONFIG
+// ============================================================
+//
+// Firebase Console
+// → Project settings
+// → Your apps
+// → Web app
+//
+// Copiá los valores que Firebase te proporciona.
+//
+// ============================================================
+
+export const firebaseConfig = {
+
+    apiKey: "PEGAR_API_KEY_AQUI",
+
+    authDomain:
+        "TU_PROJECT_ID.firebaseapp.com",
+
+    projectId:
+        "TU_PROJECT_ID",
+
+    storageBucket:
+        "TU_PROJECT_ID.firebasestorage.app",
+
+    messagingSenderId:
+        "TU_MESSAGING_SENDER_ID",
+
+    appId:
+        "TU_APP_ID"
+
+};
